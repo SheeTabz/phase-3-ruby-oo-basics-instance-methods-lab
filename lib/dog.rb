c9e0fe19fe31c383a14c32e0fbf3df9666c47c0d
SheeTabz <wmuriithitabitha@gmail.com>
2022-10-19 12:04:37 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,14 +9,3 @@ end
 
 fido = Dog.new
 fido.sit
-class Person
-    def talk
-        puts "Hello World!"
-    end
-    def walk 
-        puts "The Person is walking"
-    end
-end
-
-ada = Person.new
-ada.walk
